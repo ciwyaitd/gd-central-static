eae7c44884a7f8ddc8325f9109ae315b59200773
hejinming <b84866466@gmail.com>
2023-03-26 23:41:54 +0800

    first commit

---
+++ src/common/constants.ts
@@ -0,0 +1,112 @@
+export const nav = [
+  {
+    index: 1,
+    label: '首页',
+    link: '/index.html',
+  },
+  {
+    index: 2,
+    label: '了解法务区',
+    child: [
+      {
+        label: '组织机构',
+        link: '',
+      },
+      {
+        label: '走进法务区',
+        link: '',
+      },
+      {
+        label: '法务区视界',
+        link: '',
+      },
+      {
+        label: '白云投资集团',
+        link: '',
+      },
+    ],
+  },
+  {
+    index: 3,
+    label: '法律资源库',
+    child: [
+      {
+        label: '入驻机构',
+        link: '',
+      },
+      {
+        label: '法律人才库',
+        link: '',
+      },
+    ],
+  },
+  {
+    index: 4,
+    label: '政策指南',
+    child: [
+      {
+        label: '政策法规',
+        link: '',
+      },
+      {
+        label: '投资环境',
+        link: '',
+      },
+      {
+        label: '优惠政策',
+        link: '',
+      },
+    ],
+  },
+  {
+    index: 5,
+    label: '办事大厅',
+  },
+  {
+    index: 6,
+    label: '互动交流',
+  },
+]
+
+export const newsTab = [
+  {
+    index: 1,
+    day: '07',
+    title: '大湾资讯',
+    label: '大湾法务区',
+    date: '2023.03',
+  },
+  {
+    index: 2,
+    day: '07',
+    title: '大湾资讯',
+    label: '大湾法务区',
+    date: '2023.03',
+  },
+  {
+    index: 3,
+    day: '07',
+    title: '大湾资讯',
+    label: '大湾法务区',
+    date: '2023.03',
+  },
+]
+
+export const subjects = [
+  {
+    index: 1,
+    title: 'XX专题',
+  },
+  {
+    index: 2,
+    title: 'XX专题',
+  },
+  {
+    index: 3,
+    title: 'XX专题',
+  },
+  {
+    index: 4,
+    title: 'XX专题',
+  },
+]
